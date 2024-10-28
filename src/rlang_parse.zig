f123b5b5c9b96ed140d126fad83baca18bcc51dc
mo <mocompute@outlook.com>
2024-10-28 16:55:26 +1300

    initial tokenizer

--- src/rlang_parse.zig
+++ src/rlang_parse.zig
@@ -15,9 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-const std = @import("std");
-const StringStorage = @import("string_storage.zig").StringStorage;
-
 const Token = union(enum) {
     open_round,
     close_round,
@@ -25,6 +22,7 @@ const Token = union(enum) {
     string: []const u8,
     equal,
     comma,
+    eof,
 
     pub fn eq(self: Token, other: Token) bool {
         const Tag = std.meta.Tag(@TypeOf(self));
@@ -37,17 +35,92 @@ const Token = union(enum) {
     }
 };
 
-const TokenizeResult = union(enum) {
-    ok: []Token,
-};
+const Tokenizer = struct {
+    buffer: []const u8 = &.{},
+    index: usize = 0,
+    strings: *StringStorage,
 
-/// Caller must free returned token slice with provided allocator.
-pub fn tokenize(alloc: Allocator, source: []const u8, strings: *StringStorage) TokenizeResult {
-    _ = alloc;
-    _ = source;
-    _ = strings;
-    return .{ .ok = &.{} };
-}
+    pub fn init(buffer: []const u8, strings: *StringStorage) Tokenizer {
+        return .{ .buffer = buffer, .index = 0, .strings = strings };
+    }
+
+    pub fn deinit(self: *Tokenizer) void {
+        self.* = undefined;
+    }
+
+    const Result = union(enum) {
+        ok: Token,
+        err: union(enum) {
+            empty,
+            unterminated_string,
+            bad_token: usize,
+        },
+    };
+
+    pub fn next(self: *Tokenizer) error{OutOfMemory}!Result {
+        const State = enum {
+            start,
+            identifier,
+            string,
+        };
+        const Loc = struct {
+            start: usize,
+            end: usize,
+        };
+
+        var state: State = .start;
+        var string: Loc = .{ .start = self.index, .end = self.index };
+
+        while (self.index < self.buffer.len) {
+            const c = self.buffer[self.index];
+            const cpos = self.index;
+            self.index += 1;
+
+            switch (state) {
+                .start => switch (c) {
+                    'A'...'Z', 'a'...'z', '_', '.' => state = .identifier,
+                    '"' => state = .string,
+                    '(' => return .{ .ok = .open_round },
+                    ')' => return .{ .ok = .close_round },
+                    '=' => return .{ .ok = .equal },
+                    ',' => return .{ .ok = .comma },
+                    '\n', '\r', '\t', ' ' => continue,
+                    else => return .{ .err = .{ .bad_token = cpos } },
+                },
+                .string => switch (c) {
+                    '"' => {
+                        string.end = cpos;
+                        const s = try self.strings.append(self.buffer[string.start..string.end]);
+                        return .{ .ok = .{ .string = s } };
+                    },
+                    else => continue,
+                },
+                .identifier => switch (c) {
+                    'A'...'Z', 'a'...'z', '0'...'9', '-', '_', '.' => continue,
+                    else => {
+                        self.index -= 1; // backtrack
+                        string.end = cpos;
+                        const s = try self.strings.append(self.buffer[string.start..string.end]);
+                        return .{ .ok = .{ .identifier = s } };
+                    },
+                },
+            }
+        }
+
+        // end of input reached
+        switch (state) {
+            .start => return .{ .ok = .eof },
+            .identifier => {
+                string.end = self.index;
+                const s = try self.strings.append(self.buffer[string.start..string.end]);
+                return .{ .ok = .{ .identifier = s } };
+            },
+            .string => {
+                return .{ .err = .unterminated_string };
+            },
+        }
+    }
+};
 
 test "tokenize" {
     const alloc = std.testing.allocator;
@@ -59,13 +132,38 @@ test "tokenize" {
     var strings = try StringStorage.init(alloc, std.heap.page_allocator);
     defer strings.deinit();
 
-    const res = tokenize(alloc, source, &strings);
-    switch (res) {
-        .ok => |toks| {
-            defer alloc.free(toks);
-            try expectTokens(&.{.{ .identifier = "c" }}, toks);
-        },
+    var tokenizer = Tokenizer.init(source, &strings);
+    defer tokenizer.deinit();
+
+    try tokenizeExpect(alloc, &tokenizer, &.{
+        .{ .identifier = "c" },
+        .open_round,
+        .close_round,
+    });
+}
+
+fn tokenizeExpect(alloc: Allocator, tokenizer: *Tokenizer, expect: []const Token) !void {
+    var toks = std.ArrayList(Token).init(alloc);
+    defer toks.deinit();
+    while (true) {
+        switch (try tokenizer.next()) {
+            .ok => |tok| {
+                switch (tok) {
+                    .eof => break,
+                    else => {
+                        try toks.append(tok);
+                        // std.debug.print("Token: {}\n", .{tok});
+                    },
+                }
+            },
+            .err => |e| {
+                std.debug.print("Unexpected tokenize error: {}\n", .{e});
+                return error.TokenizeError;
+            },
+        }
     }
+
+    try expectTokens(expect, toks.items);
 }
 
 fn expectTokens(expect: []const Token, actual: []const Token) !void {
@@ -75,8 +173,13 @@ fn expectTokens(expect: []const Token, actual: []const Token) !void {
     // behaviour, so the above if test is not strictly needed for a
     // test function
     for (expect, actual) |e, a| {
-        if (!e.eq(a)) return error.TokenizeError;
+        if (!e.eq(a)) {
+            std.debug.print("Expected: {}, actual: {}\n", .{ e, a });
+            return error.ExpectFailed;
+        }
     }
 }
 
+const std = @import("std");
 const Allocator = std.mem.Allocator;
+const StringStorage = @import("string_storage.zig").StringStorage;
