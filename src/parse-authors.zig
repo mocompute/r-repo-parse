443dfd8e81d6f0db4efe963fb80c4302010a2938
mo <mocompute@outlook.com>
2024-11-08 15:53:47 +1300

    wip: create db

--- src/parse-authors.zig
+++ src/parse-authors.zig
@@ -6,13 +6,15 @@ const StringStorage = @import("string_storage.zig").StringStorage;
 const Authors = @import("Authors.zig");
 
 const Options = .{
-    .{ "help", false },
     .{ "db", 0 }, // suppress -d option
+    .{ "force", false },
+    .{ "help", false },
     .{ "verbose", false },
 };
 
 const DB_FILE = "parse-authors.db";
 var _verbose = false;
+var _force = false;
 
 const usage =
     \\Usage: parse-authors [options] <packages-file>
@@ -22,6 +24,7 @@ const usage =
     \\
     \\Options:
     \\  --db <file>    Change default SQLite3 db filename [parse-authors.db]
+    \\  --force, -f    Overwrite existing db file if it exists
     \\  --help, -h     Display help
     \\  --verbose, -v  Enable verbose logging
     \\
@@ -49,6 +52,9 @@ pub fn main() !void {
     if (options.present("verbose")) {
         _verbose = true;
     }
+    if (options.present("force")) {
+        _force = true;
+    }
 
     if (options.positional().len < 1) {
         std.debug.print("error: must provide at least one file argument\n", .{});
@@ -63,6 +69,27 @@ pub fn main() !void {
 
     log("parse-authors: using db file: {s}\n", .{db_file});
 
+    // test access to existing file
+    const file_exists = b: {
+        std.fs.cwd().access(db_file, .{}) catch |err| switch (err) {
+            error.FileNotFound => break :b false,
+            else => {},
+        };
+        break :b true;
+    };
+
+    if (!_force and file_exists) {
+        std.debug.print("error: file '{s}' already exists. Pass --force to overwrite.\n", .{db_file});
+        std.process.exit(1);
+    }
+
+    // open db
+    var conn = try mosql.Connection.open(db_file);
+    defer conn.close_wait();
+
+    // init db
+    try create_tables(&conn);
+
     // set up Authors
     var strings = try StringStorage.init(alloc, std.heap.page_allocator);
     defer strings.deinit();
@@ -70,9 +97,8 @@ pub fn main() !void {
     var authors = Authors.init(alloc);
     defer authors.deinit();
 
-    var timer = try std.time.Timer.start();
-
     // read files
+    var timer = try std.time.Timer.start();
     for (options.positional()) |file| {
         try read_file(alloc, &authors, &strings, file);
         log("Parsing '{s}' took {}ms\n", .{ file, @divFloor(timer.lap(), 1_000_000) });
@@ -81,6 +107,26 @@ pub fn main() !void {
     // summary stats
     if (_verbose)
         authors.db.debugPrintInfo();
+}
+
+fn create_tables(conn: *mosql.Connection) !void {
+    // set WAL mode and recommended security settings for untrusted databases.
+    try conn.exec(
+        \\PRAGMA journal_mode=WAL;
+        \\PRAGMA foreign_keys=1;
+        \\PRAGMA trusted_schema=0;
+        \\PRAGMA enable_view=0;
+        \\PRAGMA enable_trigger=0;
+        \\PRAGMA defensive=1;
+        \\PRAGMA dqs_dml=0;
+        \\PRAGMA dqs_ddl=0;
+    );
+    try conn.exec(
+        \\DROP TABLE IF EXISTS authors;
+        \\CREATE TABLE authors (
+        \\id INTEGER PRIMARY KEY
+        \\);
+    );
 }
 
 fn read_file(alloc: std.mem.Allocator, authors: *Authors, strings: *StringStorage, path: []const u8) !void {
@@ -102,6 +148,9 @@ fn read_file(alloc: std.mem.Allocator, authors: *Authors, strings: *StringStorag
             .warn, .err => log("{}\n", .{x}),
             .info => {},
         };
+    } else {
+        std.debug.print("error: could not read '{s}'\n", .{path});
+        return;
     }
 }
 
