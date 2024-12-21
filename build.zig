c011b68af0c2a5d60008e2f3b1126ed8b0b55877
mocompute <mocompute@outlook.com>
2024-12-21 11:04:16 -1000

    remove C static library and docs generation
    since they're not currently being developed

--- build.zig
+++ build.zig
@@ -23,18 +23,12 @@ const Module = std.Build.Module;
 const ResolvedTarget = Build.ResolvedTarget;
 const OptimizeMode = std.builtin.OptimizeMode;
 
-const targets: []const std.Target.Query = &.{
-    .{ .cpu_arch = .aarch64, .os_tag = .macos },
-    .{ .cpu_arch = .aarch64, .os_tag = .linux },
-    .{ .cpu_arch = .x86_64, .os_tag = .macos },
-    .{ .cpu_arch = .x86_64, .os_tag = .linux },
-    .{ .cpu_arch = .x86_64, .os_tag = .windows },
-};
-
 pub fn build(b: *Build) !void {
     // -- begin options ------------------------------------------------------
+
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
     // -- end options --------------------------------------------------------
 
     // -- begin dependencies -------------------------------------------------
@@ -55,53 +49,19 @@ pub fn build(b: *Build) !void {
 
     // -- end dependencies -----------------------------------------------------
 
-    // -- begin C static library -----------------------------------------------
-
-    var lib_for_docs: ?*std.Build.Step.Compile = null;
-
-    for (targets) |t| {
-        const target_ = b.resolveTargetQuery(t);
-        const lib = b.addStaticLibrary(.{
-            .name = "rrepoparse",
-            .root_source_file = b.path("src/c.zig"),
-            .target = target_,
-            .optimize = optimize,
-
-            // position independent code
-            .pic = true,
-
-            // this prevents the inclusion of stack trace printing
-            // code, which is roughly 500k.
-            // https://ziggit.dev/t/strip-option-in-build-zig/1371/8
-            .strip = true,
-        });
-        lib.linkLibC();
-
-        // just take the first one, it doesn't matter which
-        if (lib_for_docs == null) lib_for_docs = lib;
-
-        const target_out = b.addInstallArtifact(lib, .{
-            .dest_dir = .{
-                .override = .{
-                    .custom = try t.zigTriple(b.allocator),
-                },
-            },
-        });
-
-        b.getInstallStep().dependOn(&target_out.step);
-    }
-    // -- end C static library -----------------------------------------------
-
     // -- begin module -------------------------------------------------------
+
     const mod = b.addModule("r-repo-parse", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
     mod.addImport("mos", mos);
+
     // -- end module ---------------------------------------------------------
 
     // -- begin executable -------------------------------------------------------
+
     const exe = b.addExecutable(.{
         .name = "parse-authors",
         .root_source_file = b.path("src/parse-authors.zig"),
@@ -121,6 +81,7 @@ pub fn build(b: *Build) !void {
     // -- end executable ---------------------------------------------------------
 
     // -- begin test ---------------------------------------------------------
+
     const lib_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
@@ -131,14 +92,13 @@ pub fn build(b: *Build) !void {
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
+
     // -- end test -----------------------------------------------------------
 
     // -- begin check ---------------------------------------------------------
+
     const check_exe = b.addTest(.{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
@@ -148,21 +108,6 @@ pub fn build(b: *Build) !void {
 
     const check = b.step("check", "Check if root compiles.");
     check.dependOn(&check_exe.step);
+
     // -- end check -----------------------------------------------------------
-
-    // -- begin generated documentation ------------------------------------
-
-    if (lib_for_docs) |lib| {
-        const doc_step = b.step("doc", "Generate documentation");
-        const doc_install = b.addInstallDirectory(.{
-            .install_dir = .prefix,
-            .install_subdir = "doc",
-            .source_dir = lib.getEmittedDocs(),
-        });
-        doc_install.step.dependOn(&lib.step);
-        doc_step.dependOn(&doc_install.step);
-        b.getInstallStep().dependOn(&doc_install.step);
-    }
-
-    // -- end generated documentation --------------------------------------
 }
